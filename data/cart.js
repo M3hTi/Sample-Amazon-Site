2f70510c578b2048d03bed1df6067566d47ae5b6
M3hTi <mehdinasir502@yahoo.com>
2024-01-25 03:08:17 -0800

    move the addedToCart funcction into cart.js 😎

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,29 @@
 export let cart = [];
+
+
+
+/* REVIEW: baraye function e addToCart ebtada bayad befahmim k product ma to sabad kharid hast y n k age bod 
+            quantity on ro b1 vahed ezafe mikonim age nbod bayad push konim  k baraye in kar ye variable be name 
+            matchingItem dorost mikonim k bebinim be ezaye har loope k mizim aya item ma match hast ya n age bod
+            on ro to matching item miriozim*/
+export const addedToCart = function(productid){
+    let matchingItem;
+            
+        for (const item of cart) {
+            if(item.productid === productid){
+                matchingItem = item;
+                break;
+            }
+        }
+        if(matchingItem){
+            matchingItem.quantity++;
+        }else{
+                const product = {
+                    productid,
+                    quantity: 1
+                }
+                cart.push(product);
+            }
+            // console.log(cart);
+            
+}
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import{cart} from '/data/cart.js'
+import{cart, addedToCart} from '/data/cart.js'
 
 let allElement = document.querySelector('.js-all-link');
 let subAllElement = document.querySelector('.js-sub-all');
@@ -117,31 +117,8 @@ const addedProduct = function(){
     }
 }
 
-/* REVIEW: baraye function e addToCart ebtada bayad befahmim k product ma to sabad kharid hast y n k age bod 
-            quantity on ro b1 vahed ezafe mikonim age nbod bayad push konim  k baraye in kar ye variable be name 
-            matchingItem dorost mikonim k bebinim be ezaye har loope k mizim aya item ma match hast ya n age bod
-            on ro to matching item miriozim*/
-const addedToCart = function(productid){
-    let matchingItem;
 
-           for (const item of cart) {
-                if(item.productid === productid){
-                    matchingItem = item;
-                    break;
-                }
-           }
-           if(matchingItem){
-                matchingItem.quantity++;
-           }else{
-               const product = {
-                   productid,
-                   quantity: 1
-               }
-               cart.push(product);
-           }
-        //    console.log(cart);
-
-}
+ 
 
 
 // NOTE: function e update Quantity
