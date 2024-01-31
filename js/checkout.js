142bd12d502801374bbf829a3c27c0088658af3f
M3hTi <mehdinasir502@yahoo.com>
2024-01-31 02:12:40 -0800

    fix the input type radio in checkout.js file 🧹

--- js/checkout.js
+++ js/checkout.js
@@ -39,21 +39,21 @@ const showCartItem = function(){
             <div class="delivery-options">
                 <div class="delivery-option-title">Choose a Delivary option:</div>
                 <div class="delivery-option">
-                    <input type="radio" name="delivery">
+                    <input type="radio" name="delivery-${matchingProduct.id}">
                     <div>
                         <div class="delivery-option-date">Monday,February 5</div>
                         <div class="delivery-option-price">FREE Shipping</div>
                     </div>
                 </div>
                 <div class="delivery-option">
-                    <input type="radio" name="delivery">
+                    <input type="radio" name="delivery-${matchingProduct.id}">
                     <div>
                         <div class="delivery-option-date">Tuesday,january 30</div>
                         <div class="delivery-option-price">$4.99 - Shipping</div>
                     </div>
                 </div>
                 <div class="delivery-option">
-                    <input type="radio" name="delivery">
+                    <input type="radio" name="delivery-${matchingProduct.id}">
                     <div>
                         <div class="delivery-option-date">Friday, January 26</div>
                         <div class="delivery-option-price">$9.99 - Shipping</div>
